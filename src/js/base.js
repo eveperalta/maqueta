482d5cddabfe1c9eb60acf230e98e3b3d948b8c0
Eveperalta <eperalta.cm@gmail.com>
2017-03-20 18:00:12 -0300

    inicio espacios

--- src/js/base.js
+++ src/js/base.js
@@ -6,3 +6,11 @@ function myFunction() {
         x.style.display = 'none';
     }
 }
+function myFloors() { //pisos
+    var x = document.getElementById('piso');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
