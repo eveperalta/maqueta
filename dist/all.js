3c36e7900432505885ac801410b7c1b1c07a6df2
Eveperalta <eperalta.cm@gmail.com>
2017-03-21 16:26:00 -0300

    modificando index

--- dist/all.js
+++ dist/all.js
@@ -1,36 +1,3 @@
-function myFunction() {
-    var x = document.getElementById('user');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-function myFloors() { //pisos
-    var x = document.getElementById('piso');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-function mySpaces() { //pisos espacios internos
-    var x = document.getElementById('spaces2');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-function mySpacesOut() { //pisos espacios internos
-    var x = document.getElementById('spaces3');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
  //dropdown menu
  $( document ).ready(function(){
      $(".dropdown-button").dropdown();
