9b6634298d56e15ad5405cbcd753bb55bb057ea3
Eveperalta <eperalta.cm@gmail.com>
2017-03-21 18:08:52 -0300

    hasta la seccion de espacios

--- dist/all.js
+++ dist/all.js
@@ -2,6 +2,16 @@
  $( document ).ready(function(){
      $(".dropdown-button").dropdown();
  });
+ // Initialize collapse button
+ $('.button-collapse').sideNav({
+     menuWidth: 250, // Default is 300
+     edge: 'right', // Choose the horizontal origin
+     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+     draggable: true // Choose whether you can drag to open on touch screens
+   }
+ );
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
 
 /*!
  * Materialize v0.98.0 (http://materializecss.com)
--- src/js/base.js
+++ src/js/base.js
@@ -2,3 +2,13 @@
  $( document ).ready(function(){
      $(".dropdown-button").dropdown();
  });
+ // Initialize collapse button
+ $('.button-collapse').sideNav({
+     menuWidth: 250, // Default is 300
+     edge: 'left', // Choose the horizontal origin
+     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+     draggable: true // Choose whether you can drag to open on touch screens
+   }
+ );
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
